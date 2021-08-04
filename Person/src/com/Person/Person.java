ece56c6e8730379225079602b0eca88f3be170af
Bettysol <bettysolbekr@gmail.com>
2021-08-04 06:06:45 -0700

    second commit

--- Person/src/com/Person/Person.java
+++ Person/src/com/Person/Person.java
@@ -15,23 +15,21 @@ public class Person {
       this.age = age;
     }
     //
-
     public String getName() {
         return name;
     }
-
+    //
     public void setName(String name) {
         this.name = name;
     }
-
+    //
     public int getAge() {
         return age;
     }
-
+    //
     public void setAge(int age) {
         this.age = age;
     }
-//
 
 
 }
